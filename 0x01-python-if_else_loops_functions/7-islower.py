4ba587e82749c8277a9ddc21c993f6d4f370694e
Techbabe05 <betty.onils@gmail.com>
2022-12-01 09:07:17 -0800

    add python

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) > 96:
+    """Print a string in lowercase."""
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
-     else:
-         return False
+    else:
+        return False

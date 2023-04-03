c9689305b1c20c7e93370649c53ae680dfddd596
Techbabe05 <111048522+Techbabe05@users.noreply.github.com>
2023-04-03 18:50:33 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,11 @@
-#!/usr/bin/env bash
-# Script that make nginx listen to port 80
-rm /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
-service nginx restart
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status"""
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        response = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))

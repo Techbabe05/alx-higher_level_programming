26f5584af0f17b38e6e9865519f33345e409e4d2
Betty Onomiwo <betty.onils@gmail.com>
2023-05-08 17:34:20 +0000

    task 0

---
+++ 0x15-javascript-web_jquery/0-scripts.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Holberton School</title>
+  </head>
+  <body>
+    <header>First HTML Page</header>
+    <footer>HolbertonSchool -2017</footer>
+    <script type="text/javascript" src="0-script.js"></script>
+    <script>
+      document.addEventListener("DOMContentLoaded", function () {
+        const header = document.querySelector("header");
+        header.style.color = "#FF0000";
+      });
+    </script>
+  </body>
+</html>
+
